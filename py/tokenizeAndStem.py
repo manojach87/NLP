6bce3188d559ac145d13e29cfdad906ef712da20
manojach87 <manoj.ach@gmail.com>
2018-12-12 00:03:44 -0500

    .

--- py/tokenizeAndStem.py
+++ py/tokenizeAndStem.py
@@ -17,10 +17,10 @@ synopses=[]
 i=0
 for f in os.listdir(xDIR):
     #print(f)
-    if i<3 :
-        titles.append(f)
-        synopses.append(open(os.path.join(xDIR,f), encoding="utf8").read())
-        i=i+1
+    #if i<100 :
+    titles.append(f)
+    synopses.append(open(os.path.join(xDIR,f), encoding="utf8").read())
+    i=i+1
 
 #titles.append(os.path.join(xDIR,os.listdir(xDIR)[0]))
 #titles.append(os.path.join(xDIR,os.listdir(xDIR)[1]))
@@ -106,13 +106,14 @@ tfidf_matrix = tfidf_vectorizer.fit_transform(synopses) #fit the vectorizer to s
 # print(tfidf_matrix.shape)
 
 terms = tfidf_vectorizer.get_feature_names()
+terms = list(set(terms))
 
 from sklearn.metrics.pairwise import cosine_similarity
 dist = 1 - cosine_similarity(tfidf_matrix)
 
 from sklearn.cluster import KMeans
 
-num_clusters = 3
+num_clusters = 4
 
 km = KMeans(n_clusters=num_clusters)
 
@@ -152,22 +153,13 @@ for i in range(num_clusters):
     print("Cluster %d words:" % i, end='')
     
     for ind in order_centroids[i, :10]: #replace 6 with n words per cluster
-#        print('ind')
-#        print(ind)
-#        
-#        print("terms[ind]")
-#        print(terms[ind])
-#        print("terms[ind].split(' ')")
-#        print(terms[ind].split(' '))
-#        print("len(terms[ind])")
-#        print(len(terms[ind]))
+
         wordsToPass=terms[ind].split(' ')
-        print(wordsToPass)
         for word in wordsToPass:
             if checkDF(word,vocab_frame)==False:
-                wordsToPass.remove(word)
-        
-        print(wordsToPass)
+                wordsToPass = [x for x in wordsToPass if x != word]
+                #wordsToPass.remove(word)
+               #print(wordsToPass)
         
         if len(wordsToPass)>0:
         # vocab_frame.loc[terms[ind].split(' ')]
@@ -191,4 +183,161 @@ for i in range(num_clusters):
     
 print()
 print()
+
+
+
+import os  # for os.path.basename
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from sklearn.manifold import MDS
+
+MDS()
+
+# convert two components as we're plotting points in a two-dimensional plane
+# "precomputed" because we provide a distance matrix
+# we will also specify `random_state` so the plot is reproducible.
+mds = MDS(n_components=2, dissimilarity="precomputed", random_state=1)
+
+pos = mds.fit_transform(dist)  # shape (n_components, n_samples)
+
+xs, ys = pos[:, 0], pos[:, 1]
+print()
+print()
+
+#set up colors per clusters using a dict
+cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e'}
+
+#set up cluster names using a dict
+cluster_names = {0: 'Family, home, salary', 
+                 1: 'Police, killed, murders', 
+                 2: 'Friend, Father, New York, brothers', 
+                 3: 'Dance, singing, love, Humor, Humour', 
+                 4: 'Killed, soldiers, captain'}
+
+#some ipython magic to show the matplotlib plots inline
+# %matplotlib inline 
+
+#create data frame that has the result of the MDS plus the cluster numbers and titles
+df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=titles)) 
+
+#group by cluster
+groups = df.groupby('label')
+
+
+# set up plot
+fig, ax = plt.subplots(figsize=(17, 9)) # set size
+ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+
+#iterate through groups to layer the plot
+#note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
+for name, group in groups:
+    ax.plot(group.x, group.y, marker='o', linestyle='', ms=12, 
+            label=cluster_names[name], color=cluster_colors[name], 
+            mec='none')
+    ax.set_aspect('auto')
+    ax.tick_params(\
+        axis= 'x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelbottom='off')
+    ax.tick_params(\
+        axis= 'y',         # changes apply to the y-axis
+        which='both',      # both major and minor ticks are affected
+        left='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelleft='off')
+    
+ax.legend(numpoints=1)  #show legend with only 1 point
+
+#add label in x,y position with the label as the film title
+for i in range(len(df)):
+    ax.text(df.ix[i]['x'], df.ix[i]['y'], df.ix[i]['title'], size=8)  
+
+    
+    
+plt.show() #show the plot
+
+#uncomment the below to save the plot if need be
+#plt.savefig('clusters_small_noaxes.png', dpi=200)
+
+
+plt.close()
+
+#create data frame that has the result of the MDS plus the cluster numbers and titles
+df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=titles)) 
+
+#group by cluster
+groups = df.groupby('label')
+
+#define custom css to format the font and to remove the axis labeling
+css = """
+text.mpld3-text, div.mpld3-tooltip {
+  font-family:Arial, Helvetica, sans-serif;
+}
+
+g.mpld3-xaxis, g.mpld3-yaxis {
+display: none; }
+
+svg.mpld3-figure {
+margin-left: -200px;}
+"""
+
+# Plot 
+fig, ax = plt.subplots(figsize=(14,6)) #set plot size
+ax.margins(0.03) # Optional, just adds 5% padding to the autoscaling
+
+#iterate through groups to layer the plot
+#note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
+for name, group in groups:
+    points = ax.plot(group.x, group.y, marker='o', linestyle='', ms=18, 
+                     label=cluster_names[name], mec='none', 
+                     color=cluster_colors[name])
+    ax.set_aspect('auto')
+    labels = [i for i in group.title]
+    
+    #set tooltip using points, labels and the already defined 'css'
+    tooltip = mpld3.plugins.PointHTMLTooltip(points[0], labels,
+                                       voffset=10, hoffset=10, css=css)
+    #connect tooltip to fig
+    mpld3.plugins.connect(fig, tooltip, TopToolbar())    
+    
+    #set tick marks as blank
+    ax.axes.get_xaxis().set_ticks([])
+    ax.axes.get_yaxis().set_ticks([])
+    
+    #set axis as blank
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+
+    
+ax.legend(numpoints=1) #show legend with only one dot
+
+mpld3.display() #show the plot
+
+#uncomment the below to export to html
+#html = mpld3.fig_to_html(fig)
+#print(html)        
+
+
+from scipy.cluster.hierarchy import ward, dendrogram
+
+linkage_matrix = ward(dist) #define the linkage_matrix using ward clustering pre-computed distances
+
+fig, ax = plt.subplots(figsize=(15, 20)) # set size
+ax = dendrogram(linkage_matrix, orientation="right", labels=titles);
+
+plt.tick_params(\
+    axis= 'x',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom='off',      # ticks along the bottom edge are off
+    top='off',         # ticks along the top edge are off
+    labelbottom='off')
+
+plt.tight_layout() #show plot with tight layout
+
+#uncomment below to save figure
+plt.savefig('ward_clusters.png', dpi=200) #save figure as ward_clusters
 
